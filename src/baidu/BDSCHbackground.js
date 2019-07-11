5562fb93573411cc7fa6de1bc6cdd0d94f3db218
igsnow <zzy716810918@gmail.com>
2019-07-11 15:38:28 +0800

    小度搜索

--- src/baidu/BDSCHbackground.js
+++ src/baidu/BDSCHbackground.js
@@ -1,5 +1,5 @@
 chrome.contextMenus.create({
-    title: '使用度娘搜索：%s',
+    title: '使用小度搜索：%s',
     contexts: ['selection'],
     onclick: function (params) {
         chrome.tabs.create({url: 'https://www.baidu.com/s?ie=utf-8&wd=' + encodeURI(params.selectionText)});
