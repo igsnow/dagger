fdaa7c4c51cd4bc534b35f2da5fbca46ad3f6482
igsnow <zzy716810918@gmail.com>
2019-07-14 14:31:59 +0800

    添加测试js

---
+++ src/test/test.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false,                     // 是否显示浏览器
+        args: ['--start-maximized']          // 是否全屏显示
+    });
+    const page = await browser.newPage();
+    await page.evaluateOnNewDocument(() => {
+        Object.defineProperty(navigator, 'webdriver', {
+            get: () => undefined,
+        });
+    })
+    await page.setViewport({
+        width: 1500,
+        height: 900
+    });
+
+    let detailUrl = 'https://detail.1688.com/offer/585943639773.html?spm=a261p.8650809.0.0.6e476328Bt7AQv&tracelog=cps&clickid=e4f7eaf54a50c5a293338d5f39f68907';
+    await page.goto(detailUrl, {
+        waitUntil: 'load'
+    });
+
+
+})();
