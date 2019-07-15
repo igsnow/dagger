623e6d93c7bc6ea7d185fd71782024d27c1a9c84
igsnow <zzy716810918@gmail.com>
2019-07-15 15:47:46 +0800

    后台可以向chrome插件发送采购信息

--- src/baidu/BDSCHbackground.js
+++
@@ -1,7 +0,0 @@
-chrome.contextMenus.create({
-    title: '使用小度搜索：%s',
-    contexts: ['selection'],
-    onclick: function (params) {
-        chrome.tabs.create({url: 'https://www.baidu.com/s?ie=utf-8&wd=' + encodeURI(params.selectionText)});
-    }
-});
---
+++ src/baidu/background.js
@@ -0,0 +1,19 @@
+chrome.contextMenus.create({
+    title: '使用小度搜索：%s',
+    contexts: ['selection'],
+    onclick: function (params) {
+        chrome.tabs.create({url: 'https://www.baidu.com/s?ie=utf-8&wd=' + encodeURI(params.selectionText)});
+    }
+});
+
+chrome.runtime.onMessageExternal.addListener(function (request, sender, sendResponse) {
+    let msg = JSON.stringify(request)
+    alert(msg)
+    // 可以针对sender做一些白名单检查
+    // sendResponse返回响应
+    if (request.type == 'MsgFromPage') {
+        sendResponse({tyep: 'MsgFromChrome', msg: 'Hello, I am chrome res~'});
+    }
+});
+
+
