33578e3b0fd38fa6f8bec1d21fec3abfdf81d689
igsnow <zzy716810918@gmail.com>
2019-07-16 10:39:32 +0800

    åå批量è两点击跳转到相应的1688页面

--- src/baidu/background.js
+++ src/baidu/background.js
@@ -2,13 +2,16 @@
 window.data = [];
 
 chrome.runtime.onMessageExternal.addListener(function (request, sender, sendResponse) {
-    let msg = JSON.stringify(request)
-    alert(msg)
     window.data.push(request)
+    let d = JSON.stringify(window.data)
+
+    window.open(request.msg.url)
+
     // 可以针对sender做一些白名单检查
     if (request.type == 'MsgFromPage') {
         sendResponse({type: 'MsgFromChrome', msg: 'Hello, I am chrome res~'});
     }
 });
+
 
 
