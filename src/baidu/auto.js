e63bebd7a6079dce068db1a7e0ad3880c38c9e02
igsnow <zzy716810918@gmail.com>
2019-07-17 00:05:47 +0800

    jquery $ 未不是一个函数的问题修复

--- src/baidu/auto.js
+++ src/baidu/auto.js
@@ -3,14 +3,15 @@ function sendMessageToContentScriptByPostMessage(type, data) {
     window.postMessage({cmd: type, data: data}, '*');
 }
 
-function get1688() {
+function get1688(jQuery) {
     //获取抓取的所有信息，然后返回给content
     var data = {name: 'zzy'}
 
     // document.getElementsByClassName("amount-up ")[0].click()
     // $(".amount-up").click()
-    // ('.list-leading')
-    $(".region-detail-title").css("background-color", "red")
+    // console.log($('.list-leading'))
+
+    // $(".region-detail-title").css("background-color", "red")
 
     sendMessageToContentScriptByPostMessage("1688", data)
 }
@@ -19,7 +20,7 @@ function get1688() {
 // 通过DOM事件发送消息给content-script
 (function (jQuery) {
     if (location.host == 'detail.1688.com') {
-        get1688();
+        get1688(jQuery);
     } else {
         alert(location.host + "下单功能还未加入!")
     }
