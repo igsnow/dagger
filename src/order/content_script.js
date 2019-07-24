c05c8e876c079fee79762d14dac51d6435b3ddb8
igsnow <zzy716810918@gmail.com>
2019-07-24 19:11:05 +0800

    蒙层只加载一次

--- src/order/content_script.js
+++ src/order/content_script.js
@@ -32,10 +32,9 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
     if (location.host == 'detail.1688.com') {
         if (request.cmd == 'pre') {
-            console.log(request.value);
             if (request.value == null) return;
             $(document).ready(function () {
-                preMask()
+                loadOnceMask()
             })
         }
 
@@ -51,10 +50,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             // 过滤掉非采购1688页面
             if (request.value == null) return;
             // 如果预加载蒙层没出现(有概率失败)，如果没有则标签加载完成再加上蒙层
-            let isMaskExit = document.getElementById('bgMask');
-            if (isMaskExit == null) {
-                preMask()
-            }
+            loadOnceMask();
             getActionTip(skuObj, num, itemImg, itemName);
 
             // 如果有SKU更多展开按钮，则点击
@@ -165,6 +161,270 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     } else {
         console.log('不是1688页面!')
     }
+    if (location.host == 'item.taobao.com') {
+        if (request.cmd == 'pre') {
+            if (request.value == null) return;
+            $(document).ready(function () {
+                // 预加载蒙层
+                loadOnceMask()
+            })
+        }
+
+        if (request.cmd == 'sku') {
+            console.log(request.value);
+            let res = request.value;
+            skuObj = res && res.skuProps && JSON.parse(res.skuProps);
+            num = res && res.skuNum;
+            itemImg = res && res.img;
+            itemName = res && res.name;
+            sendResponse('sku消息已收到！');
+
+            // 过滤掉非采购1688页面
+            if (request.value == null) return;
+            // 如果预加载蒙层没出现(有概率失败)，如果没有则标签加载完成再加上蒙层
+            loadOnceMask();
+            getActionTip(skuObj, num, itemImg, itemName);
+
+            // 如果有SKU更多展开按钮，则点击
+            let hasMore = $(".obj-expand");
+            if (hasMore && hasMore[0]) {
+                let isShow = hasMore[0].style.display;
+                if (!!isShow) {
+                    hasMore[0].click();
+                    console.log('=>展开更多sku列表...')
+                }
+            }
+            // 判断页面是否有如颜色等切换的SKU属性 即含有class为obj-leading的标签
+            let hasObjLead = $('.obj-leading');
+            if (hasObjLead.length) {
+                hasFirstSku = true;
+                // 匹配页面的第一个sku属性名称
+                let firstSkuEle = $('.obj-leading .obj-title');
+                if (firstSkuEle && firstSkuEle[0]) {
+                    let firstSkuName = firstSkuEle[0].innerHTML;
+                    let name = getSkuValByName(firstSkuName, skuObj);
+                    firstSku = skuObj[name];
+                }
+                let aList = $('.list-leading a');
+                // let arr = [];
+                for (let i = 0; i < aList.length; i++) {
+                    // arr.push(aList[i].title);
+                    if (aList[i].title == firstSku) {
+                        first_index = i;
+                        break
+                    }
+                }
+                // console.log({arr, first_index});
+                // 拿到头部sku下标，开始点击
+                if (first_index !== undefined) {
+                    if ($('.list-leading a') && $('.list-leading a')[first_index]) {
+                        $('.list-leading a')[first_index].click();
+                        console.log('=>头部sku已选!')
+                    }
+                }
+            }
+            // 是否有数量选择sku
+            let hasObjSku = $('.obj-sku');
+            if (hasObjSku.length) {
+                // 匹配页面的第二个sku属性名称
+                let secondSkuEle = $('.obj-sku .obj-title');
+                if (secondSkuEle && secondSkuEle[0]) {
+                    let secondSkuName = secondSkuEle[0].innerHTML;
+                    let name = getSkuValByName(secondSkuName, skuObj);
+                    secondSku = skuObj[name];
+                }
+                let nList = $('.table-sku .name');
+                // let arr = [];
+                for (let i = 0; i < nList.length; i++) {
+                    // 如果sku有图片，则sku取span标签的title值
+                    if (nList[i].children[0].title) {
+                        // arr.push(nList[i].children[0].title);
+                        if (nList[i].children[0].title == secondSku) {
+                            second_index = i;
+                            break
+                        }
+                    } else {
+                        // arr.push(nList[i].children[0].innerHTML);
+                        if (nList[i].children[0].innerHTML == secondSku) {
+                            second_index = i;
+                            break
+                        }
+                    }
+                }
+                // console.log({arr, second_index});
+                if (second_index !== undefined) {
+                    second_index += 1;
+                    // (坑)自动填写商品数量，但是下方价格不改变，于是先自增一再减一，价格正确显示
+                    let ipt = $('.table-sku tr:nth-child(' + second_index + ') .amount-input');
+                    let up = $('.table-sku tr:nth-child(' + second_index + ') .amount-up');
+                    let down = $('.table-sku tr:nth-child(' + second_index + ') .amount-down');
+                    if (ipt && ipt[0]) {
+                        ipt[0].value = num;
+                    }
+                    if (up && up[0]) {
+                        up[0].click()
+                    }
+                    if (down && down[0]) {
+                        down[0].click()
+                    }
+                    console.log('=>数量已自动填充完成!');
+                }
+            }
+            // 点击加入购物车按钮
+            let cart = $('.do-cart');
+            // 如果有头部sku,只有头部下标有值才能点击加入购物车
+            if (hasFirstSku && first_index !== undefined && second_index !== undefined) {
+                cart[0].click();
+            } else if (second_index !== undefined) {
+                cart[0].click();
+            }
+            // sleep3秒钟，若出现提示框，则说明加入购物车成功
+            setTimeout(() => {
+                let successDialog = $('.purchase-dialog');
+                if (successDialog.length) {
+                    if (successDialog.css("display") != 'none') {
+                        console.log('=>加入购物车成功!');
+                        let tipDiv = document.getElementById('cartTip');
+                        tipDiv.innerHTML = '加入购物车成功!';
+                    }
+                }
+            }, 1500)
+        }
+    } else {
+        console.log('不是淘宝页面!')
+    }
+    if (location.host == 'detail.tmall.com') {
+        if (request.cmd == 'pre') {
+            if (request.value == null) return;
+            $(document).ready(function () {
+                // 预加载蒙层
+                loadOnceMask()
+            })
+        }
+
+        if (request.cmd == 'sku') {
+            console.log(request.value);
+            let res = request.value;
+            skuObj = res && res.skuProps && JSON.parse(res.skuProps);
+            num = res && res.skuNum;
+            itemImg = res && res.img;
+            itemName = res && res.name;
+            sendResponse('sku消息已收到！');
+
+            // 过滤掉非采购1688页面
+            if (request.value == null) return;
+            // 如果预加载蒙层没出现(有概率失败)，如果没有则标签加载完成再加上蒙层
+            loadOnceMask();
+            getActionTip(skuObj, num, itemImg, itemName);
+
+            // 如果有SKU更多展开按钮，则点击
+            let hasMore = $(".obj-expand");
+            if (hasMore && hasMore[0]) {
+                let isShow = hasMore[0].style.display;
+                if (!!isShow) {
+                    hasMore[0].click();
+                    console.log('=>展开更多sku列表...')
+                }
+            }
+            // 判断页面是否有如颜色等切换的SKU属性 即含有class为obj-leading的标签
+            let hasObjLead = $('.obj-leading');
+            if (hasObjLead.length) {
+                hasFirstSku = true;
+                // 匹配页面的第一个sku属性名称
+                let firstSkuEle = $('.obj-leading .obj-title');
+                if (firstSkuEle && firstSkuEle[0]) {
+                    let firstSkuName = firstSkuEle[0].innerHTML;
+                    let name = getSkuValByName(firstSkuName, skuObj);
+                    firstSku = skuObj[name];
+                }
+                let aList = $('.list-leading a');
+                // let arr = [];
+                for (let i = 0; i < aList.length; i++) {
+                    // arr.push(aList[i].title);
+                    if (aList[i].title == firstSku) {
+                        first_index = i;
+                        break
+                    }
+                }
+                // console.log({arr, first_index});
+                // 拿到头部sku下标，开始点击
+                if (first_index !== undefined) {
+                    if ($('.list-leading a') && $('.list-leading a')[first_index]) {
+                        $('.list-leading a')[first_index].click();
+                        console.log('=>头部sku已选!')
+                    }
+                }
+            }
+            // 是否有数量选择sku
+            let hasObjSku = $('.obj-sku');
+            if (hasObjSku.length) {
+                // 匹配页面的第二个sku属性名称
+                let secondSkuEle = $('.obj-sku .obj-title');
+                if (secondSkuEle && secondSkuEle[0]) {
+                    let secondSkuName = secondSkuEle[0].innerHTML;
+                    let name = getSkuValByName(secondSkuName, skuObj);
+                    secondSku = skuObj[name];
+                }
+                let nList = $('.table-sku .name');
+                // let arr = [];
+                for (let i = 0; i < nList.length; i++) {
+                    // 如果sku有图片，则sku取span标签的title值
+                    if (nList[i].children[0].title) {
+                        // arr.push(nList[i].children[0].title);
+                        if (nList[i].children[0].title == secondSku) {
+                            second_index = i;
+                            break
+                        }
+                    } else {
+                        // arr.push(nList[i].children[0].innerHTML);
+                        if (nList[i].children[0].innerHTML == secondSku) {
+                            second_index = i;
+                            break
+                        }
+                    }
+                }
+                // console.log({arr, second_index});
+                if (second_index !== undefined) {
+                    second_index += 1;
+                    // (坑)自动填写商品数量，但是下方价格不改变，于是先自增一再减一，价格正确显示
+                    let ipt = $('.table-sku tr:nth-child(' + second_index + ') .amount-input');
+                    let up = $('.table-sku tr:nth-child(' + second_index + ') .amount-up');
+                    let down = $('.table-sku tr:nth-child(' + second_index + ') .amount-down');
+                    if (ipt && ipt[0]) {
+                        ipt[0].value = num;
+                    }
+                    if (up && up[0]) {
+                        up[0].click()
+                    }
+                    if (down && down[0]) {
+                        down[0].click()
+                    }
+                    console.log('=>数量已自动填充完成!');
+                }
+            }
+            // 点击加入购物车按钮
+            let cart = $('.do-cart');
+            // 如果有头部sku,只有头部下标有值才能点击加入购物车
+            if (hasFirstSku && first_index !== undefined && second_index !== undefined) {
+                cart[0].click();
+            } else if (second_index !== undefined) {
+                cart[0].click();
+            }
+            // sleep3秒钟，若出现提示框，则说明加入购物车成功
+            setTimeout(() => {
+                let successDialog = $('.purchase-dialog');
+                if (successDialog.length) {
+                    if (successDialog.css("display") != 'none') {
+                        console.log('=>加入购物车成功!');
+                        let tipDiv = document.getElementById('cartTip');
+                        tipDiv.innerHTML = '加入购物车成功!';
+                    }
+                }
+            }, 1500)
+        }
+    } else {
+        console.log('不是天猫页面!')
+    }
 });
 
 // 给sku属性名做兼容，比如页面元素是返回尺码（双），带个单位，但接口返回'尺码'，做个兼容
@@ -190,7 +450,7 @@ function preMask() {
     bgDiv.style.right = "0";
     bgDiv.style.bottom = "0";
     bgDiv.style.opacity = "0.8";
-    bgDiv.style.zIndex = "2002";
+    bgDiv.style.zIndex = "999999999";
     document.body.appendChild(bgDiv);
     let closeBtn = document.createElement("div");
     closeBtn.setAttribute("id", "cBtn");
@@ -238,7 +498,7 @@ function getActionTip(sku, num, img, name) {
     infoDiv.style.top = "150px";
     infoDiv.style.right = "120px";
     infoDiv.style.opacity = "0.8";
-    infoDiv.style.zIndex = "2003";
+    infoDiv.style.zIndex = "9999999999";
     document.body.appendChild(infoDiv);
     // 采购单sku详情
     let skuDiv = document.createElement("div");
@@ -276,6 +536,14 @@ function getActionTip(sku, num, img, name) {
     infoDiv.appendChild(tipDiv);
     // 如果开始采购，则弹框信息显示加载中，否则显示加入购物车成功
     tipDiv.innerHTML = '采购中......';
+}
+
+// 蒙层只加载一次
+function loadOnceMask() {
+    let isMaskExit = document.getElementById('bgMask');
+    if (isMaskExit == null) {
+        preMask()
+    }
 }
 
 
